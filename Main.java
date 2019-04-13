c0b88169e55d603505bbe25d93c2cfccf0a2d7da
Ant1pov <49588027+Ant1pov@users.noreply.github.com>
2019-04-13 15:33:44 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
-package DZ_2;
+package DZ_1;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Culkul culkul = new Culkul();
-        System.out.println(culkul.viet(1,8,15));
-        culkul.fibo();
-        culkul.tabl();
-        culkul.elka1();
-        culkul.elka2();
-        culkul.vremya(4);
-        culkul.chisla1(4,125);
-        culkul.chisla2(4, 54);
-        culkul.bilet();
+        culkul.diskriment();
+        culkul.krug();
+        culkul.dlina();
+        culkul.treugol(4, 7);
     }
 }
